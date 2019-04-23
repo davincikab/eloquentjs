e8065f430fee1d052ba543322281884609d441bb
davincikab <davidnganganjer079@gmail.com>
2019-04-23 10:52:40 +0300

    exceptions handling and error propagation

--- scp/chap8.js
+++ scp/chap8.js
@@ -40,10 +40,39 @@ function numberToString(n, base = 10){
     result = String(n % base) + result;
     n = Math.floor(n/base);
     console.log( Math.floor(n/base));
-    
+
   }while (n > 0);
 
   return sign + result;
 }
 
 console.log(numberToString(13, 10));
+
+
+// ERROR propagation
+// Exception handling
+function promptDirection(question){
+  let result = prompt(question);
+
+  if(result.toLowerCase() == 'left') return 'L';
+  if(result.toLowerCase() == 'right') return 'R';
+
+  throw new Error("Invlid direction: " + result);
+}
+
+
+function look() {
+  if(promptDirection("Which way ?") == 'L'){
+    return "A house";
+  }
+
+  else {
+    return " Two birds";
+  }
+}
+
+try {
+  console.log(look());
+} catch (e) {
+  console.log(e);
+}
