90dc8a27d04cbe712054630181da268226458ea5
davincikab <davidnganganjer079@gmail.com>
2019-04-23 10:27:25 +0300

    Manual Debugging

--- scp/chap8.js
+++ scp/chap8.js
@@ -29,3 +29,21 @@ test("covert text to uppercase", ()=>{
 
 
 // DEBUGGING
+function numberToString(n, base = 10){
+  let result = "", sign = "";
+  if(n < 0){
+    sign = "-";
+    n = - n;
+  }
+
+  do{
+    result = String(n % base) + result;
+    n = Math.floor(n/base);
+    console.log( Math.floor(n/base));
+    
+  }while (n > 0);
+
+  return sign + result;
+}
+
+console.log(numberToString(13, 10));
