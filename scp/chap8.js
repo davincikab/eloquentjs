86daeca3cdb88ddc9ccd9a10f056c5ade1f4ae3a
davincikab <davidnganganjer079@gmail.com>
2019-04-23 09:59:04 +0300

    Testing a program output

--- scp/chap8.js
+++ scp/chap8.js
@@ -1,9 +1,31 @@
 // Using strict mode
 "use strict";
 function iterate(){
-  for (i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     console.log(i);
   }
 }
 
 iterate();
+
+// Use of new keyword in strict mode
+function Person(name){this.name = name; }
+let davi = new Person('David');
+console.log(davi.name);
+
+// testing
+
+function test(label, body){
+  if(!body()) console.log(`Failed ${label}`);
+}
+
+test("covert text to uppercase", ()=>{
+  return "hello".toUpperCase() == 'HELLO';
+});
+
+test("covert text to uppercase", ()=>{
+  return "hello".toUpperCase() == 'Hello';
+});
+
+
+// DEBUGGING
